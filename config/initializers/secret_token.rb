15c453f219e5aad9e4b14b0684d4f6d73ed9d4fc
Jack <jack.miszencin@gmail.com>
2013-03-28 14:20:05 -0400

    Before creating sessions object

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrackHunter::Application.config.secret_token = '23f77629ef6d0aeed597562b74ff73361a64d0a8ca45d5906f352a1f3dffa8f9796d043645fcc680283ea11c638ce165426e000f92466b256c4712baea31fb87'
