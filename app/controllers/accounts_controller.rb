7031f2017d1ab61b307f3479b01f0c07e48e0992
Jack <jack.miszencin@gmail.com>
2013-04-16 12:38:56 -0400

    GPS and logout now functioning fully.

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -24,6 +24,8 @@ class AccountsController < ApplicationController
   
   
   def logout
+    session[:user_id] = nil
+    redirect_to root_path
   end
   
 end
---
+++ db/migrate/20130416163809_add_is_admin_and_is_merchant_to_user.rb
@@ -0,0 +1,4 @@
+class AddIsAdminAndIsMerchantToUser < ActiveRecord::Migration
+  def change
+  end
+end
