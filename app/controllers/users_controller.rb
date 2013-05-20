795308627147134d90754f4b33c1fd46c16f997f
Jack <jack.miszencin@gmail.com>
2013-05-20 14:21:24 -0400

    Eliminated merchant_selection from users controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,6 @@
 class Users::SessionsController < Devise::SessionsController
 
-#Custom material starts here.
 
-	def merchant_selection
-		@user = @current_user
-		@merchants = Merchant.where(:lng => (@user.lng_low)..(@user.lng_up), :lat => (@user.lat_low)..(@user.lat_up))
-	end
 	def home
 		@user = @current_user
 		if request.post?
@@ -19,11 +14,4 @@ class Users::SessionsController < Devise::SessionsController
 			end
 		end
 	end
-#	def new
-#		if request.post?
-#				redirect_to home_path(current_user)  
-#		else
-#		end
-#	end
-
 end
